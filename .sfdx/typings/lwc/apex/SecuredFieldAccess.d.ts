094edf19e0b6db1619f831735f5a47156f54754a
Sriram Venkatraman <sriram.venkatraman@salesforce.com>
2022-01-14 19:55:00 -0500

    Remove newly ignored directory ".sfdx"

--- .sfdx/typings/lwc/apex/SecuredFieldAccess.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/SecuredFieldAccess.getFields" {
-  export default function getFields(param: {fieldList: any, recordId: any, sObjectName: any}): Promise<any>;
-}
-declare module "@salesforce/apex/SecuredFieldAccess.updateField" {
-  export default function updateField(param: {fieldName: any, value: any, recordId: any, sObjectName: any}): Promise<any>;
-}
